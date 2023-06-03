af5fd3a04d2d7cfcdbc8fd8ffdc9868a8ff9009f
k355l3r-5yndr0m3 <hoanghung17jan@gmail.com>
2023-06-04 01:25:10 +0700

    Plugin

---
+++ xla/pjrt/plugin/plugin.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stddef.h>
+#include "xla/pjrt/c/pjrt_c_api.h"
+
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+const PJRT_Api* GetPjrtApi();
+const void *SerializeCompileOptions(size_t *compile_options_size);
+
+#ifdef __cplusplus 
+}
+#endif
+
